5b30da4c04b38acb78d75fe5b21bb941ad52448d
parsley1400 <55346686+parsley1400@users.noreply.github.com>
2020-02-14 15:40:34 +0900

    firstcommit

---
+++ circle.js
@@ -0,0 +1,27 @@
+$(".cell1").click(function () {
+    $(".cell").css("color","#001f76").css("background","#fff")
+    $(this).css("background","#001f76").css("color","#fff")
+    $(".article").css("display","none");
+    $(".article1").css("display","block");
+});
+
+$(".cell2").click(function () {
+    $(".cell").css("color","#001f76").css("background","#fff")
+    $(this).css("background","#001f76").css("color","#fff")
+    $(".article").css("display","none");
+    $(".article2").css("display","block");
+});
+
+$(".cell3").click(function () {
+    $(".cell").css("color","#001f76").css("background","#fff")
+    $(this).css("background","#001f76").css("color","#fff")
+    $(".article").css("display","none");
+    $(".article3").css("display","block");
+});
+
+$(".cell4").click(function () {
+    $(".cell").css("color","#001f76").css("background","#fff")
+    $(this).css("background","#001f76").css("color","#fff")
+    $(".article").css("display","none");
+    $(".article4").css("display","block");
+});
